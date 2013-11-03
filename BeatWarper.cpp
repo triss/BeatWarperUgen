60f45271c80d23df72928aae9d06f73dc0cb4716
Tristan Strange <tristan.strange@gmail.com>
2013-11-03 19:04:53 +0000

    Added fades between slices, interpolation, and ability to change playback

--- BeatWarper.cpp
+++ BeatWarper.cpp
@@ -1,34 +1,79 @@
 #include "SC_PlugIn.h"
 #include <stdio.h>
 
+using namespace std; // for math functions
 
-// Ugen argument positions
-#define BUFFER 0
-#define LENGTH 1
-#define FADE_IN 2
-#define FADE_OUT 3
-#define SLICE_LENGTH 4
-
-#define SLICE_OFFSET 5
 
 
 static InterfaceTable *ft;
 
+////////////////////////////////////////////////////////////////////////////////
+// lifted from /server/plugins/DelayUGens.cpp
+////////////////////////////////////////////////////////////////////////////////
 
-// declare struct to hold unit generator state
+// checks if buffer is loaded properly
+static inline bool checkBuffer(Unit * unit, const float * bufData, uint32 bufChannels,
+							   uint32 expectedChannels, int inNumSamples)
+{
+	if (!bufData)
+		goto handle_failure;
+
+	if (expectedChannels > bufChannels) {
+		if(unit->mWorld->mVerbosity > -1 && !unit->mDone)
+			Print("Buffer UGen channel mismatch: expected %i, yet buffer has %i channels\n",
+				  expectedChannels, bufChannels);
+		goto handle_failure;
+	}
+	return true;
+
+handle_failure:
+	unit->mDone = true;
+	ClearUnitOutputs(unit, inNumSamples);
+	return false;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+// Ugen argument positions
+#define BUFFER 0
+#define WARPED_LENGTH 1
+#define RATE 2
+#define SLICE_LENGTH 3
+#define FADE_IN 4
+#define FADE_OUT 5
+
+#define SLICE_OFFSET 6
+
+// handles wrapping around in buffer when we need to interpolate beginning and
+// end of file - only really useful when no fade in and sliceLength = 1 
+inline double wrapAround(Unit *unit, double in, double hi)
+{
+	if (in >= hi) {
+		in -= hi;
+		if (in < hi) return in;
+	} else if (in < 0.) {
+		in += hi;
+		if (in >= 0.) return in;
+	} else return in;
+
+	return in - hi * floor(in/hi);
+}
+
+// define a struct to hold unit generator state
 struct BeatWarper : public Unit
 {
 	uint32 warpPos;		// position in warper - at destination tempo
-	uint32 bufferPos;	// position in buffer
+	double phase;		// position in buffer - double to handle interpolation 
+						// nicely
 
-	int prevSlice; 		// current slice number
-	int numSlices; 		// number of slices
+	double slicePos; 			// the current frame in a slice
+	int prevSlice; 				// the number of the last slice to be played
+	int numSlices; 				// number of slices
+	double currentSliceStart; 	// the start position of the current slice
+	int fadeOutPos;
 
-	bool fadingIn, fadingOut; 	// whether a fade in/out is in progress
-	int fadeInPos, fadeOutPos; 	// how far through fade in/out we are
-	uint32 fadeOutBufferPos; 	// buffer position that last fade out started
-
-	// buffer storage - utilised by the GET_BUF_SHARED macro
+	// buffer storage - utilised by the ACQUIRE_SNDBUF_SHARED macro
+	// hence the ugly m_ names
 	SndBuf *m_buf;
 	float m_fbufnum;
 };
@@ -42,96 +87,177 @@ static void BeatWarper_Ctor(BeatWarper* unit);
 // constructor called upon ugen startup
 void BeatWarper_Ctor(BeatWarper* unit)
 {
-	// set the calculation function
+	// set the calculation function - no one really wants to Warp a control 
+	// rate buffer do they? I've just done one at audio rate
 	SETCALC(BeatWarper_next_a);
 
 	// initialise unit generator state
 	unit->warpPos = 0;
 	unit->prevSlice = 0;
-	unit->bufferPos = 0;
+	unit->phase = 0;
 
 	unit->numSlices = unit->mNumInputs - SLICE_OFFSET;
 
     // calculate one sample of output - every ugen needs to do this
-	BeatWarper_next_a(unit, 1);
+    // the bufferisn't likely tobe ready it seems sowe'll just output a
+	// sample of silence
+	ClearUnitOutputs(unit, 1);
 }
 
 
-// calculation function for an audio rate frequency argument
+// calculation function for audio rate 
 void BeatWarper_next_a(BeatWarper *unit, int inNumSamples)
 {
-	// read in length ugen arg at control rate
-    int length = (int)(IN0(LENGTH) * SAMPLERATE); 	
-
-	int fadeInTime = (int)(IN0(FADE_IN) * length);
-	int fadeOutTime = (int)(IN0(FADE_OUT) * length);
-
-	// apparently taking reciprocal and multiplying is quicker than dividing 
-	// so we do this for length here
-	double lengthRecip = 1.0f / length; 	
-
-	// lookup current position, number of slices from unit
+	// lookup current position in warper, slice and buffer as well as number 
+	// of slices from unit
 	uint32 warpPos = unit->warpPos;
 	int prevSlice = unit->prevSlice;
 	int numSlices = unit->numSlices;
+	double slicePos = unit->slicePos;
+	double currentSliceStart = unit->currentSliceStart;
+	int fadeOutPos = unit->fadeOutPos;
 
-	// load buffer
-	GET_BUF_SHARED
-	
-	// check the buffer has loaded if not return
-	if (!bufData || ((bufFrames & ((unit->mWorld->mBufLength<<1) - 1)) != 0)) {
-		unit->bufferPos = 0;
-		ClearUnitOutputs(unit, inNumSamples);
-		printf("ERROR: Buffer not available");
+	// get our position in the buffer
+	double phase = unit->phase;
+
+	// read in warped length ugen arg at control rate
+    int warpedLength = (int)(IN0(WARPED_LENGTH) * SAMPLERATE); 	
+
+	// apparently taking reciprocal and multiplying is quicker than dividing 
+	// over and over so we do this for length here
+	double warpedLengthRecip = 1.0f / warpedLength; 	
+
+	float rate = IN0(RATE);
+
+	double sliceLengthSamples = 
+		IN0(SLICE_LENGTH) * (warpedLength / numSlices);
+
+	float fadeInSamples = IN0(FADE_IN);
+	float fadeOutSamples = IN0(FADE_OUT);
+
+	// load in the buffer if its changed
+	float fbufnum  = ZIN0(0);
+	if (fbufnum != unit->m_fbufnum) {
+		uint32 bufnum = (int)fbufnum;
+		World *world = unit->mWorld;
+		if (bufnum >= world->mNumSndBufs) bufnum = 0;
+		unit->m_fbufnum = fbufnum;
+		unit->m_buf = world->mSndBufs + bufnum;
+	}
+
+	// acquire the buffer for reading
+	const SndBuf *buf = unit->m_buf;
+	ACQUIRE_SNDBUF_SHARED(buf);
+	const float *bufData __attribute__((__unused__)) = buf->data;
+	uint32 bufChannels __attribute__((__unused__)) = buf->channels;
+	uint32 bufSamples __attribute__((__unused__)) = buf->samples;
+	uint32 bufFrames = buf->frames;
+	int mask __attribute__((__unused__)) = buf->mask;
+	int guardFrame __attribute__((__unused__)) = bufFrames - 2;
+
+	// check buffer has been acquired
+	int numOutputs = unit->mNumOutputs;
+	if (!checkBuffer(unit, bufData, bufChannels, numOutputs, inNumSamples))
 		return;
+
+	// if we've reached the end of the buffer go back to the beginning of it
+	if (phase >= bufFrames) {
+		phase = 0;
 	}
 
-	// check num outputs is same as buffers channels if not return
-	if (unit->mNumOutputs != bufChannels) { 
-		ClearUnitOutputs(unit, inNumSamples); 
-		printf("ERROR: Channel outputs != Buffer outputs")
-		return; 
-	} 
-
-	// TODO fix wonkiness when shorter buffer loaded!!!
-	if (unit->bufferPos >= bufFrames) {
-		unit->bufferPos = unit->bufferPos % bufFrames;
-	}
-
-	// shift bufData pointer by current bufferPos * number of channels
-	uint32 bufferPos = unit->bufferPos;
-	bufData += bufferPos * bufChannels;
-
-	int slice = 0;
+	// probably best not to keep setting aside this space!
+	// we'll just update the value sin here
+	int currentSlice;
+	double fadeAmt = 1.;
 
     // perform a loop for the number of samples in the control period
     for (int i=0; i < inNumSamples; ++i) {
 		// calculate current slice number
-		slice = warpPos * lengthRecip * numSlices;
+		currentSlice = warpPos * warpedLengthRecip * numSlices;
 		
 		// if the slice has changed
-		if(prevSlice != slice) { 	
+		if(prevSlice != currentSlice) { 	
 			// jump to that slice's position
-			bufferPos = IN0(slice + SLICE_OFFSET) * bufFrames;
-			prevSlice = slice;
+			currentSliceStart = IN0(currentSlice + SLICE_OFFSET) * bufFrames;
+			phase = currentSliceStart;
+			prevSlice = currentSlice;
+			slicePos = 0.0;
+			fadeOutPos = 0;
 		}
 
-        // write the output for each channel in the buffer and shift the
-		// buffer pointer
-		for (uint32 c = 0; c < bufChannels; ++c) {
-			// TODO Interpolate here?
-			OUT(c)[i] = *bufData++;
+		// calculate fade in/out amount
+		
+		// if we're fading in
+		if(slicePos < fadeInSamples) {
+			fadeAmt = pow(slicePos / fadeInSamples, 4);
 		}
 
-		// TODO Interpolate here also?
-		bufferPos = (bufferPos + 1) % bufFrames;
-		warpPos = (warpPos + 1) % length;
+		if(slicePos >= sliceLengthSamples - fadeOutSamples) {
+			fadeAmt	= 1.0 - pow(fadeOutPos / fadeOutSamples, 4);
+
+			if(fadeAmt < 0.0) {
+				fadeAmt = 0.0;
+			}
+
+			fadeOutPos += 1;
+		}
+
+		// look up positions of 4 samples to interpolate
+		phase = wrapAround((Unit*)unit, phase, bufFrames); 
+		int32 iphase = (int32)phase; 
+		const float* bPos = bufData + iphase * bufChannels; 
+		const float* aPos = bPos - bufChannels; 
+		const float* cPos = bPos + bufChannels; 
+		const float* dPos = cPos + bufChannels; 
+
+		// change positions if we're about to loop around
+		// only really useful if no fade times are set
+		if (iphase == 0) { 
+			aPos += bufSamples; 
+		} else if (iphase >= guardFrame) { 
+			if (iphase == guardFrame) { 
+				dPos -= bufSamples; 
+			} else { 
+				cPos -= bufSamples; 
+				dPos -= bufSamples; 
+			} 
+		} 
+
+		// work out how far between samples we'd like to be
+		float fracphase = phase - (double)iphase; 
+
+		// interpolate each channel and output
+		for (int32 channel=0; channel<numOutputs; ++channel) { 
+			// look up the samples we're going to interpolate
+			float a = aPos[channel]; 
+			float b = bPos[channel]; 
+			float c = cPos[channel]; 
+			float d = dPos[channel]; 
+
+			// interpolate them, apply fade and output them
+			OUT(channel)[i] = cubicinterp(fracphase, a, b, c, d) * fadeAmt; 
+		}
+
+		phase += rate;
+
+		slicePos = phase - currentSliceStart;
+		if(slicePos < 0.) { 
+			slicePos = 0.;
+		}
+
+
+		warpPos = (warpPos + 1) % warpedLength;
     }
 
-    // store the phase back to the struct
+	RELEASE_SNDBUF_SHARED(buf);
+
+    // store the phase back to the struct so we can look it up next cycle
     unit->warpPos = warpPos;
-	unit->bufferPos = bufferPos;
+	unit->phase = phase;
+	unit->slicePos = slicePos;
     unit->prevSlice = prevSlice;
+	unit->currentSliceStart = currentSliceStart;
+	unit->fadeOutPos = fadeOutPos;
 }
 
 
--- sc/BeatWarper.sc
+++ sc/BeatWarper.sc
@@ -1,18 +1,24 @@
 BeatWarper : MultiOutUGen {
 	*ar { 
 		arg numChannels, bufnum, length = 1, 
+		rate = 1,
 		slices, sliceLength = 1, 
 		fadeIn = 10, fadeOut = 10, 
 		mul = 1, add = 0;
 
 		^this.multiNew(
 			'audio', numChannels, bufnum, 
-			length, sliceLength, 
+			length, rate, sliceLength, 
 			fadeIn, fadeOut, 
-			*slices
+
+			// slices needs to come last since ugen code receives 
+			// arrays as a list of arguments
+			*slices 
 		).madd(mul, add);
 	}
 
+	// ensure number of channels gets handled 
+	// properly by buffer handling macro
 	init { arg numChannels ... theInputs;
 		inputs = theInputs;
 		^this.initOutputs(numChannels, 'audio');
